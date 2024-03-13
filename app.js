c1af528af8113e836e2b78f39439ee7c0ececb3f
RushikeshChamle <rushikeshchamle23@gmail.com>
2024-03-14 00:36:22 +0530

    added port changes

--- app.js
+++ app.js
@@ -94,7 +94,7 @@ app.get("/replay/:sessionId", (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
