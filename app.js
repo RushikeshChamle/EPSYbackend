574bdb10dc4eacf7635b715e8c5405d404fded04
RushikeshChamle <rushikeshchamle23@gmail.com>
2024-03-14 05:18:54 +0530

    added userfetch api

--- app.js
+++ app.js
@@ -260,4 +260,26 @@ app.post("/projects", (req, res) => {
   );
 });
 
+app.get("/users/:userId", (req, res) => {
+  const userId = req.params.userId;
+
+  // Query to fetch user data from the database
+  const SELECT_USER_QUERY = "SELECT * FROM users WHERE id = ?";
+  connection.query(SELECT_USER_QUERY, [userId], (err, results) => {
+    if (err) {
+      console.error("Error retrieving user:", err);
+      res.status(500).json({ message: "Internal server error" });
+      return;
+    }
+
+    if (results.length === 0) {
+      res.status(404).json({ message: "User not found" });
+      return;
+    }
+
+    const user = results[0];
+    res.status(200).json(user);
+  });
+});
+
 module.exports = app;
